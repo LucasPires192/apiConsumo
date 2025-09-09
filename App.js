bf6d8ff3d12cc298d4f92872651895b47b7f2bcf
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-09 07:33:24 -0300

    feat: criacao do app e da estilizacao

--- App.js
+++ App.js
@@ -1,6 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, FlatList } from 'react-native';
+import style from './style.js';
+
+const request = async (callback) => {
+    const response = await fetch('https://swapi.dev/api/people/');
+    const parsed = await response.json();
+    callback(parsed.results);
+}
+
 export default function App() {
-  return (
-    <>
-    </>
-  );
+    const [registros, setRegistros] = useState([]);
+
+    useEffect(() => {
+        request(setRegistros);
+    }, [])
+
+    return (
+        <View style={style.container}>
+            <View>
+                <Text style={style.superior}>
+                    StarWars
+                </Text>
+            </View>
+
+            <FlatList
+                data={registros}
+                keyExtractor={(item) => item.name.toString()}
+                renderItem={({item}) =>
+                    <Text style={style.item}>
+                        <Text>Nome: {item.name}{'\n'}</Text>
+                        <Text>Peso: {item.mass}{'\n'}</Text>
+                    </Text>
+                }
+            />
+        </View>
+    );
 }
