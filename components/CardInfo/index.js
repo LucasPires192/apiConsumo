dde72176c990ebae822a8ba63a1ddffe11f00f29
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-10 07:34:55 -0300

    chore: inicio da edicao do estilo do component CardInfo

--- components/CardInfo/index.js
+++ components/CardInfo/index.js
@@ -1,16 +1,23 @@
 import { View, Image, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import style from './style.js';
 
 const CardInfo = ({imagem, nome, tipo}) => {
     return(
-        <View style={style.item}>
+        <LinearGradient
+            colors={['#B2933E', '#B2933E', '#372E0B']}
+            style={style.card_container}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            locations={[0.1, 0.25, 0.8]}
+        >
             <Image 
                 source={{uri: imagem}}
                 style={style.card_image}
             />
-            <Text style={style.nome}>Nome: {nome}{'\n'}</Text>
-            <Text style={style.tipo}>Tipo: {tipo}{'\n'}</Text>
-        </View>
+            <Text style={style.card_nome}>Nome: {nome}{'\n'}</Text>
+            <Text style={style.card_tipo}>Tipo: {tipo}{'\n'}</Text>
+        </LinearGradient>
     )
 }
 
--- components/CardInfo/style.js
+++ components/CardInfo/style.js
@@ -1,20 +1,18 @@
 import { StyleSheet } from 'react-native';
 
 const style = StyleSheet.create({
-    item: {
-        backgroundColor: '#fff',
+    card_container:{
         flex: 1,
-        paddingVertical: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flexDirection: 'row',
         marginBottom: 10,
         marginHorizontal: 10,
-    },
-    nome: {
-        marginTop: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        overflow: 'hidden',
     },
     card_image: {
-        height: 200,
+        height: 300,
         width: '100%',
         resizeMode: 'contain',
     }
