0f2a11d3020ab4a63fd7ee6700ec7dceba957a64
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-09-10 23:54:04 -0300

    chore: customizacao do card de informacoes das cartas

--- components/CardInfo/index.js
+++ components/CardInfo/index.js
@@ -7,16 +7,28 @@ const CardInfo = ({imagem, nome, tipo}) => {
         <LinearGradient
             colors={['#B2933E', '#B2933E', '#372E0B']}
             style={style.card_container}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
+            start={{ x: 1, y: 1 }}
+            end={{ x: 0, y: 0 }}
             locations={[0.1, 0.25, 0.8]}
         >
-            <Image 
-                source={{uri: imagem}}
-                style={style.card_image}
-            />
-            <Text style={style.card_nome}>Nome: {nome}{'\n'}</Text>
-            <Text style={style.card_tipo}>Tipo: {tipo}{'\n'}</Text>
+            <View style={style.card_image_container}>
+                <Image 
+                    source={{uri: imagem}}
+                    style={style.card_image}
+                />
+            </View>
+            <View style={style.card_info_container}>
+                <Text style={style.card_info}>
+                    <Text style={style.card_label}>
+                        Nome:
+                    </Text>
+                    {'  '}{nome}{'\n'}
+                    <Text style={style.card_label}>
+                        Tipo:
+                    </Text>
+                    {'  '}{tipo}{'\n'}
+                </Text>
+            </View>
         </LinearGradient>
     )
 }
--- components/CardInfo/style.js
+++ components/CardInfo/style.js
@@ -3,19 +3,30 @@ import { StyleSheet } from 'react-native';
 const style = StyleSheet.create({
     card_container:{
         flex: 1,
-        flexDirection: 'row',
+        gap: 10,
         marginBottom: 10,
         marginHorizontal: 10,
         paddingVertical: 10,
         paddingHorizontal: 10,
         borderRadius: 10,
         overflow: 'hidden',
+        alignItems: 'center',
+    },
+    card_image_container: {
+        height: 320,
+        width: 200,
+        overflow: 'hidden',
     },
     card_image: {
-        height: 300,
+        height: '100%',
         width: '100%',
         resizeMode: 'contain',
-    }
+    },
+    card_label: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 800,
+    },
 })
 
 export default style;
